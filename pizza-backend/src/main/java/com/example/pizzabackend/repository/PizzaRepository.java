4e2a2e6b226678eb673f513c135db1ed11b2d74a
Gerardo Lopez <gelopfalcon@gmail.com>
2018-07-09 20:49:56 -0600

    Update pizza repository

--- pizza-backend/src/main/java/com/example/pizzabackend/repository/PizzaRepository.java
+++ pizza-backend/src/main/java/com/example/pizzabackend/repository/PizzaRepository.java
@@ -3,6 +3,13 @@ package com.example.pizzabackend.repository;
 import com.example.pizzabackend.domain.Pizza;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+
 public interface PizzaRepository extends MongoRepository<Pizza, String> {
 
+    Optional<List<Pizza>> findByPrice(int price);
+
+    Optional<Pizza> findByName(String name);
 }
