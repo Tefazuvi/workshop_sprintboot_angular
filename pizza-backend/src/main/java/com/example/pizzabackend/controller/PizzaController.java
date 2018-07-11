d6484548cdba1385293555c842eb2f9a2f9f9ff8
Gerardo Lopez <gelopfalcon@gmail.com>
2018-07-11 17:58:15 -0600

    update controller

--- pizza-backend/src/main/java/com/example/pizzabackend/controller/PizzaController.java
+++ pizza-backend/src/main/java/com/example/pizzabackend/controller/PizzaController.java
@@ -1,13 +1,15 @@
 package com.example.pizzabackend.controller;
 
+import com.example.pizzabackend.domain.Pizza;
 import com.example.pizzabackend.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 @RestController
@@ -19,12 +21,31 @@ public class PizzaController {
     @GetMapping(
             value = "/pizza",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getPizzas(){
+    public ResponseEntity<?> getPizzas() {
         Optional result = pizzaService.getAllPizzas();
-        if (result.isPresent()) {
-            return new ResponseEntity<>(result.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(ResponseEntity.notFound(), HttpStatus.NOT_FOUND);
-        }
+        return result.isPresent() ? new ResponseEntity<>(result.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(value = "/pizza",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> insertPizza(@Valid @RequestBody Pizza pizzaDto) {
+
+        pizzaService.insertPizza(pizzaDto);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/pizza/{pizzaId}")
+    public ResponseEntity<?> deletePizza(@PathVariable("pizzaId") String pizzaId) {
+        pizzaService.deletePizzaById(pizzaId);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @PatchMapping(value = "/pizza/{pizzaId}")
+    public ResponseEntity<?> updatePizzaByPrice(@PathVariable("pizzaId") String id, @RequestParam("price") int price) {
+        Optional result = pizzaService.updatePizza(id, price);
+        return result.isPresent() ? new ResponseEntity<>(result.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
